b5078b676e4e430d0ed3526f0a175e1802c1c274
palash trivedi <thinkert26@gmail.com>
2023-07-21 13:17:06 +0530

    backend base update

---
+++ horizon-backend/src/main/java/com/horizonbackend/HorizonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.horizonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonBackendApplication.class, args);
+	}
+
+}
---
+++ horizon-backend/src/test/java/com/horizonbackend/HorizonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
